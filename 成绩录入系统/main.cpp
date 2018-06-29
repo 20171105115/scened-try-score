5fa8110d3657b652ce2c3580aa2e27e9baf35906
朱博宇 <s20171105115@chu1e1adeimac.local>
2018-06-29 20:33:18 +0800

    Initial Commit

---
+++ 成绩录入系统/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  成绩录入系统
+//
+//  Created by 朱博宇 on 2018/6/29.
+//  Copyright © 2018年 朱博宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
