23079ce8e0d61ee848a95b2de26ac5a1df016da0
朱博宇 <s20171105115@chu1e1adeiMac.local>
2018-07-01 17:54:03 +0800

    test success
    实现了文件数据的分行输入，用了链表及指针。

--- 成绩录入系统/main.cpp
+++ 成绩录入系统/main.cpp
@@ -5,18 +5,18 @@
 #include<stdlib.h>
 
 struct Stu{
- char Number;
- char Name;
- char Sex;
- int Birth;
- char Class;
- int Phone;
- int Judge1;
- int Judge2;
- int Judge3;
- int Judge4;
- int Judge5;
-    struct Stu *next;
+ char Number[20];
+ char Name[20];
+ char Sex[20];
+ char Birth[20];
+ char Class[30];
+ char Phone[20];
+ int Judge1[5];
+ int Judge2[5];
+ int Judge3[5];
+ int Judge4[5];
+ int Judge5[5];
+ struct Stu *next;
  };
 int main(){
 int min,max,average=0;
@@ -30,7 +30,7 @@ struct Stu *head,*p;
     p=head;
     FILE *s;
     FILE *b;
-    b=fopen("//Users/s20171105115/Desktop/test.csv","a");
+    b=fopen("/Users/s20171105115/Desktop/test.csv","a");
     if((s=fopen("/Users/s20171105115/Desktop/studentdata.csv","r"))==0)
     {
         printf("文件不存在\n");
@@ -38,10 +38,13 @@ struct Stu *head,*p;
     else{
         for(i=0;i<n;i++){
             while(p!=0){
-            fscanf(s,"%s ,%s ,%s ,%d ,%s ,%d ,%d ,%d ,%d ,%d ,%d",&a[i].Number,&a[i].Name,&a[i].Sex,&a[i].Birth,&a[i].Class,&a[i].Phone,&a[i].Judge1,&a[i].Judge2,&a[i].Judge3,&a[i].Judge4,&a[i].Judge5);
-            fprintf(b,"%s ,%s ,%s ,%d ,%s ,%d ,%d ,%d ,%d ,%d ,%d",p->Number,p->Name,p->Sex,p->Birth,p->Class,p->Phone,p->Judge1,p->Judge2,p->Judge3,p->Judge4,p->Judge5);
-            printf("%s %s %s %s %s %s %s %s %s %s %s\n",p->Number,p->Name,p->Sex,p->Birth,p->Class,p->Phone,p->Judge1,p->Judge2,p->Judge3,p->Judge4,p->Judge5);
+            fscanf(s,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ",a[i].Number,a[i].Name,a[i].Sex,a[i].Birth,a[i].Class,a[i].Phone,a[i].Judge1,a[i].Judge2,a[i].Judge3,a[i].Judge4,a[i].Judge5);
+                 printf("%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s",p->Number,p->Name,p->Sex,p->Birth,p->Class,p->Phone,p->Judge1,p->Judge2,p->Judge3,p->Judge4,p->Judge5);
+                printf("\n");
+                fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%d ,%d ,%d ,%d ,%d\n",p->Number,p->Name,p->Sex,p->Birth,p->Class,p->Phone
+                ,p->Judge1,p->Judge2,p->Judge3,p->Judge4,p->Judge5);
                 p=p->next;
+                break;
             }
         }
     }
