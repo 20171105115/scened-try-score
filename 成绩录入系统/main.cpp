cde837de3ec1987de6f9e03fb84223273bc44155
朱博宇 <s20171105115@chu1e1adeiMac.local>
2018-07-06 18:36:01 +0800

    解决了结构体二维数组问题
    解决了结构体二维数组问题，但是在字符串的加减上出现了问题，字符串加减转数字有问题

--- 成绩录入系统/main.cpp
+++ 成绩录入系统/main.cpp
@@ -21,7 +21,7 @@ struct Stu{
  };
 int main(){
 int min,max,average=0;
-int i,n,t=0,n1,m,j;
+int i,n,t=0,n1,m,j=0;
     scanf("%d %d",&n,&m);
 struct Stu a[n][m];
 struct Stu *head,*p;
@@ -39,10 +39,10 @@ struct Stu *head,*p;
         for(i=0;i<n;i++){
             for(j=0;j<m;j++){
                 if(p!=0){
-                   fscanf(s,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s",a[i][j].Number,a[i][j].Name,a[i][j].Sex,a[i][j].Birth,a[i][j].Class,a[i][j].Phone,a[i][j].Judge1,a[i][j].Judge2,a[i][j].Judge3,a[i][j].Judge4,a[i][j].Judge5);
+                   fscanf(s,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s",&a[i][j].Number,&a[i][j].Name,&a[i][j].Sex,&a[i][j].Birth,&a[i][j].Class,&a[i][j].Phone,&a[i][j].Judge1,&a[i][j].Judge2,&a[i][j].Judge3,&a[i][j].Judge4,&a[i][j].Judge5);
                    printf("%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s",a[i][j].Number,a[i][j].Name,a[i][j].Sex,a[i][j].Birth,a[i][j].Class,a[i][j].Phone,a[i][j].Judge1,a[i][j].Judge2,a[i][j].Judge3,a[i][j].Judge4,a[i][j].Judge5);
                    printf("\n");
-                   printf("%d\n",p->Judge1-'0');
+                   printf("%s\n",a[i][j].Judge1);
                    fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s\n",a[i][j].Number,a[i][j].Name,a[i][j].Sex,a[i][j].Birth,a[i][j].Class,a[i][j].Phone,a[i][j].Judge1,a[i][j].Judge2,a[i][j].Judge3,a[i][j].Judge4,a[i][j].Judge5);
                    p=p->next;
                    break;
