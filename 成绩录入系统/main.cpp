e911d3c365d239c0db2891abab3e5caf520a30a4
朱博宇 <s20171105115@chu1e1adeiMac.local>
2018-07-07 12:46:03 +0800

    基本实现功能
    基本上实现了从文件中输入数据，计算数据，打分的功能，并输出到其他文件中，但是现在评委的个数只能固定，预计变动评委人数需要动态分配的功能

--- 成绩录入系统/main.cpp
+++ 成绩录入系统/main.cpp
@@ -10,23 +10,28 @@ struct Stu{
  char Sex[20];
  char Birth[20];
  char Class[30];
- char Phone[20];
- int Judge1[5];
- int Judge2[5];
- int Judge3[5];
- int Judge4[5];
- int Judge5[5];
- int *p;
+ char Phone[30];
+ char judge1[10];
+ char judge2[10];
+ char judge3[10];
+ char judge4[10];
+ char judge5[10];
+ int Judge1;
+ int Judge2;
+ int Judge3;
+ int Judge4;
+ int Judge5;
+ int average;
  struct Stu *next;
  };
 int main(){
-int min,max,average=0;
-int i,n,t=0,n1,m,j=0;
-    scanf("%d %d",&n,&m);
-struct Stu a[n][m];
+int average=0;
+int i=0,n,t=0,j=0;
+scanf("%d",&n);
+struct Stu a[n];
 struct Stu *head,*p;
-    head=&a[t][0];
-    a[t][0].next=&a[t+1][0];
+    head=&a[t];
+    a[t].next=&a[t+1];
     p=head;
     FILE *s;
     FILE *b;
@@ -36,18 +41,50 @@ struct Stu *head,*p;
         printf("文件不存在\n");
     }
     else{
+        fscanf(s,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s",&a[i].Number,&a[i].Name,&a[i].Sex,&a[i].Birth,&a[i].Class,&a[i].Phone,&a[i].judge1,&a[i].judge2,&a[i].judge3,&a[i].judge4,&a[i].judge5);
+        printf("numbers,name,sex,dateofbirth,class,phoneNo,judge1,judge2,judge3,judge4,judge5,average\n");
+        fprintf(b,"numbers,name,sex,dateofbirth,class,phoneNo,judge1,judge2,judge3,judge4,judge5,average\n");
+        p=p->next;
         for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                if(p!=0){
-                   fscanf(s,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s",&a[i][j].Number,&a[i][j].Name,&a[i][j].Sex,&a[i][j].Birth,&a[i][j].Class,&a[i][j].Phone,&a[i][j].Judge1,&a[i][j].Judge2,&a[i][j].Judge3,&a[i][j].Judge4,&a[i][j].Judge5);
-                   printf("%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s",a[i][j].Number,a[i][j].Name,a[i][j].Sex,a[i][j].Birth,a[i][j].Class,a[i][j].Phone,a[i][j].Judge1,a[i][j].Judge2,a[i][j].Judge3,a[i][j].Judge4,a[i][j].Judge5);
-                   printf("\n");
-                   printf("%s\n",a[i][j].Judge1);
-                   fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s\n",a[i][j].Number,a[i][j].Name,a[i][j].Sex,a[i][j].Birth,a[i][j].Class,a[i][j].Phone,a[i][j].Judge1,a[i][j].Judge2,a[i][j].Judge3,a[i][j].Judge4,a[i][j].Judge5);
+            while(p!=0){
+                   fscanf(s,"%s ,%s ,%s ,%s ,%s ,%s ,%d ,%d ,%d ,%d ,%d",&a[i].Number,&a[i].Name,&a[i].Sex,&a[i].Birth,&a[i].Class,&a[i].Phone,&a[i].Judge1,&a[i].Judge2,&a[i].Judge3,&a[i].Judge4,&a[i].Judge5);
                    p=p->next;
                    break;
-                }
             }
         }
     }
+    int max[20],min[20];
+    for(i=0;i<n;i++)
+    {
+        max[i]=min[i]=a[i].Judge1;
+        if(a[i].Judge2>max[i])
+            max[i]=a[i].Judge2;
+        if(a[i].Judge3>max[i])
+            max[i]=a[i].Judge3;
+        if(a[i].Judge4>max[i])
+            max[i]=a[i].Judge4;
+        if(a[i].Judge5>max[i])
+            max[i]=a[i].Judge5;
+        if(a[i].Judge2<min[i])
+            min[i]=a[i].Judge2;
+        if(a[i].Judge3<min[i])
+            min[i]=a[i].Judge3;
+        if(a[i].Judge4<min[i])
+            min[i]=a[i].Judge4;
+        if(a[i].Judge5<min[i])
+            min[i]=a[i].Judge5;
+    }
+    for(i=0;i<n;i++)
+    {
+        a[i].average=(a[i].Judge1+a[i].Judge2+a[i].Judge3+a[i].Judge4+a[i].Judge5-max[i]-min[i])/3;
+    }
+    i=0;
+    while(i<n)
+    {
+        printf("%s/%s/%s/%s/%s/%s/%d/%d/%d/%d/%d/%d\n",a[i].Number,a[i].Name,a[i].Sex,a[i].Birth,a[i].Class,a[i].Phone,a[i].Judge1,a[i].Judge2,a[i].Judge3,a[i].Judge4,a[i].Judge5,a[i].average);
+        fprintf(b,"%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d,%d\n",
+                a[i].Number,a[i].Name,a[i].Sex,a[i].Birth,a[i].Class,a[i].Phone,a[i].Judge1,a[i].Judge2,a[i].Judge3,a[i].Judge4,a[i].Judge5,a[i].average);
+        i++;
+    }
+
 }
